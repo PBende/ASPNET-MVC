b0c2caa06a4619b1f09338164b9a1ac60362f1ea
pbende <pavanibende@gmail.com>
2021-10-12 11:16:17 +0530

    notes and updates

--- dbfirstassets/App_Start/BundleConfig.cs
+++ dbfirstassets/App_Start/BundleConfig.cs
@@ -12,7 +12,8 @@ namespace dbfirstassets
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"
+                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -25,6 +26,7 @@ namespace dbfirstassets
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
         }
     }
 }
